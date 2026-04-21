1cab353a907b87c36325bc9db245203cafd62e8b
Anas <anasalwardtech@gmail.com>
2026-04-21 04:43:20 +0300

    feat: add project tasks component with filtering and task management features
    feat: implement projects sidebar for navigation between project details
    
    feat: create recent activity component to display task updates
    
    feat: develop sidebar component for main navigation
    
    feat: add stats grid component to summarize project statistics
    
    feat: implement tasks summary component for user-specific task overview
    
    feat: create workspace dropdown for selecting and managing workspaces
    
    chore: setup Tailwind CSS for styling
    
    chore: initialize main entry point for React application
    
    chore: configure TypeScript for application
    
    chore: setup Vite for development environment

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <h1 className="text-red-800">Welcome to the React-Redux Template!</h1>
+      </section>
+
+      
+    </>
+  )
+}
+
+export default App
